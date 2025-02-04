1393fc38e95f89c62a7931401ccdf314d77a89de
Sasi Bhushan Kumar Peruri <sasi.bhushan.kumar.peruri@ibm.com>
2025-02-04 14:48:28 +0000

    04/02- final changes

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -8,6 +8,16 @@ sap.ui.define([
         },
         onPress: function(){
             this.getOwnerComponent().getRouter().navTo("RouteView2");
-           }
+           },
+        onSubmit: function(){
+          var name =  this.getView().byId('_IDGenInput').getValue();
+          var msg = "Welcome to " + name;
+          this.getView().byId('_IDGenText1').setText(msg);
+          this.getView().byId('_IDGenButton').setType('Reject');
+          this.getView().byId('_IDGenText1').setTextAlign('Left');
+          this.getView().byId('_IDGenInput').setEnabled(false);
+          // to navigate to View 2
+          this.getOwnerComponent().getRouter().navTo("RouteView2");
+        }   
     });
 });
